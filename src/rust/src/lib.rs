ee2da21a0a6a75eba3558d857242bc4e6b88b8e8
Hiroaki Yutani <yutani.ini@gmail.com>
2024-05-16 22:32:36 +0900

    Move to a separate crate

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,3 +1,5 @@
+mod expr;
+
 use std::sync::Arc;
 
 use arrow::{
@@ -12,6 +14,7 @@ use datafusion::{
     execution::context::SessionContext,
     logical_expr::{col, lit, Expr},
 };
+use expr::RExprs;
 use pollster::block_on;
 use savvy::{r_eprintln, r_println, savvy, Sexp, StringSexp, TypedSexp};
 
@@ -120,94 +123,5 @@ impl RDataFrame {
             .select(exprs.0)
             .map_err(|e| <savvy::Error>::from(e.to_string()))?;
         Ok(Self::new(new_df))
-    }
-}
-
-#[savvy]
-struct RExpr(Expr);
-
-#[savvy]
-impl RExpr {
-    fn print(&self) -> savvy::Result<()> {
-        r_println!("{}", self.0.to_string());
-        Ok(())
-    }
-
-    fn col(x: &str) -> savvy::Result<Self> {
-        Ok(Self(col(x)))
-    }
-
-    fn lit(x: Sexp) -> savvy::Result<Self> {
-        let lit = match &x.into_typed() {
-            TypedSexp::Integer(i) => lit(i.as_slice()[0]),
-            TypedSexp::Real(r) => lit(r.as_slice()[0]),
-            TypedSexp::Logical(l) => lit(l.iter().next().unwrap()),
-            TypedSexp::String(s) => lit(s.iter().next().unwrap()),
-            _ => return Err("Unsupported type".into()),
-        };
-        Ok(Self(lit))
-    }
-
-    fn add(self, rhs: RExpr) -> savvy::Result<Self> {
-        Ok(Self(self.0 + rhs.0))
-    }
-
-    fn sub(self, rhs: RExpr) -> savvy::Result<Self> {
-        Ok(Self(self.0 - rhs.0))
-    }
-
-    fn mul(self, rhs: RExpr) -> savvy::Result<Self> {
-        Ok(Self(self.0 * rhs.0))
-    }
-
-    fn div(self, rhs: RExpr) -> savvy::Result<Self> {
-        Ok(Self(self.0 / rhs.0))
-    }
-
-    fn modulo(self, rhs: RExpr) -> savvy::Result<Self> {
-        Ok(Self(self.0 % rhs.0))
-    }
-
-    fn lt(self, rhs: RExpr) -> savvy::Result<Self> {
-        Ok(Self(self.0.lt(rhs.0)))
-    }
-
-    fn lt_eq(self, rhs: RExpr) -> savvy::Result<Self> {
-        Ok(Self(self.0.lt_eq(rhs.0)))
-    }
-
-    fn gt(self, rhs: RExpr) -> savvy::Result<Self> {
-        Ok(Self(self.0.gt(rhs.0)))
-    }
-
-    fn gt_eq(self, rhs: RExpr) -> savvy::Result<Self> {
-        Ok(Self(self.0.gt_eq(rhs.0)))
-    }
-
-    fn eq(self, rhs: RExpr) -> savvy::Result<Self> {
-        Ok(Self(self.0.eq(rhs.0)))
-    }
-
-    fn not_eq(self, rhs: RExpr) -> savvy::Result<Self> {
-        Ok(Self(self.0.not_eq(rhs.0)))
-    }
-
-    fn alias(self, name: &str) -> savvy::Result<Self> {
-        Ok(Self(self.0.alias(name)))
-    }
-}
-
-#[savvy]
-struct RExprs(Vec<Expr>);
-
-#[savvy]
-impl RExprs {
-    fn new(capacity: usize) -> savvy::Result<Self> {
-        Ok(Self(Vec::with_capacity(capacity)))
-    }
-
-    fn add_expr(&mut self, expr: RExpr) -> savvy::Result<()> {
-        self.0.push(expr.0);
-        Ok(())
     }
 }
